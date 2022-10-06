61e7678c29a85a8ec1e32f64c1245032389545b4
Ajwahib95 <wrecktington@gmail.com>
2022-10-06 17:29:44 +0100

    add carousel to main branch index.js

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import Head from "next/head";
-
+import Carousel from "../src/components/Carousel/Carousel"
 
 export default function Home() {
   return (
+    <>
       <Head>
         <title>E-com</title>
       </Head>  
+      <Carousel />
+    </>  
   );
 }

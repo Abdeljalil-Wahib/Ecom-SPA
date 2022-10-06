5ad0b9c618c9cc4fa168ee48b4ea741c2d365ac7
Ajwahib95 <wrecktington@gmail.com>
2022-10-06 17:37:33 +0100

    test2

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Head from "next/head";
-
+import Carousel from "../src/components/Carousel/Carousel"
 
 export default function Home() {
   return (
@@ -7,6 +7,7 @@ export default function Home() {
       <Head>
         <title>E-com</title>
       </Head>
+      <Carousel />
     </>    
   );
 }

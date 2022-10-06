a4d53c887ee430317ae0b29d49aaa49a4ea50710
Ajwahib95 <wrecktington@gmail.com>
2022-10-06 17:31:50 +0100

    test

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,10 @@ import Head from "next/head";
 
 export default function Home() {
   return (
+    <>
       <Head>
         <title>E-com</title>
-      </Head>  
+      </Head>
+    </>    
   );
 }

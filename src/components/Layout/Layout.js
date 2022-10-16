1f9138de1018e2c4c846f71726892297614a7656
jbouazao <jihad.bo2@gmail.com>
2022-10-16 13:51:07 +0100

    created the store setup and adjusted the state accordingly

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,20 +1,17 @@
 import { useState } from "react";
+import { useSelector } from "react-redux";
 import Cart from "../Cart/Cart";
 import Navbar from "../Navbar/Navbar";
 import classes from "./Layout.module.css";
 
 const Layout = ({ children }) => {
-	const [showCart, setshowCart] = useState(false)
-
-	const cartShowHandler = () => {
-		setshowCart(prev => !prev)
-	}
+	const showCart = useSelector(state => state.ui.cartIsVisible)
 	
   return (
     <div className={classes.Layout}>
-			{showCart && <Cart cartClicked = {cartShowHandler}/>}
+			{showCart && <Cart/>}
 			<div className = {classes.innerlayout}>
-				<Navbar cartClicked = {cartShowHandler}/>
+				<Navbar />
 				{children}
 			</div>
     </div>

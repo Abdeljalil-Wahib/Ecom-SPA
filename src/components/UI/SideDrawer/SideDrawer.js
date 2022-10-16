b133376dded74d72eac149f0f6686737a190c66b
Ajwahib95 <wrecktington@gmail.com>
2022-10-16 20:52:21 +0100

    added CardproductCard component

--- src/components/UI/SideDrawer/SideDrawer.js
+++ src/components/UI/SideDrawer/SideDrawer.js
@@ -1,10 +1,10 @@
 import React from "react";
 import classes from "../SideDrawer/SideDrawer.module.css";
 
-const SideDrawer = ({ ClickHandler }) => {
+const SideDrawer = ({ ClickHandler, children }) => {
   return (
     <div className={classes.container}>
-      <div className={classes.sideDrawer}></div>
+      <div className={classes.sideDrawer}>{children}</div>
       <div onClick={ClickHandler} className={classes.backdrop}></div>
     </div>
   );

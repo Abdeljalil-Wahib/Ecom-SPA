e2b703ec657e5902bd29338ed1fd9f65a43384db
Ajwahib95 <wrecktington@gmail.com>
2025-08-19 17:32:59 +0100

    fix: Correct critical errors preventing Vercel deployment

--- src/components/MembershipForm/Membership.js
+++ src/components/MembershipForm/Membership.js
@@ -6,7 +6,7 @@ const Membership = () => {
   return (
     <div className={classes.container}>
       <h2 className={classes.h2}>Explore our Membership</h2>
-      <p className={classes.para}>Get that new clothes feeling every month with a monthly membership. We have a plan that's right for you and your budget.</p>
+      <p className={classes.para}>Get that new clothes feeling every month with a monthly membership. We have a plan that&apos;s right for you and your budget.</p>
       <Link href="" className={classes.linkBtn}>
         LEARN MORE
       </Link>
